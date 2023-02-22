abbebe2eda5f23087023622202a9e5ab52d4a5e6
LRudL <rudolf.laine@gmail.com>
2023-02-22 15:43:00 +0000

    Add BasicCNN for MNIST training

--- dots/models.py
+++ dots/models.py
@@ -2,7 +2,6 @@ from dots.dots import JModule
 from dots.utils import flatten
 import torch as t
 
-
 class MLP(JModule):
     def __init__(
         self, in_size, out_size,
@@ -22,3 +21,32 @@ class MLP(JModule):
     
     def forward(self, x):
         return self.layers(x)
+
+class BasicCNN(JModule):
+    def __init__(
+        self,
+        in_size = (28, 28) # MNIST
+    ):
+        # currently hard_coded to work only with MNIST sizes
+        
+        super().__init__()
+        self.layers = t.nn.Sequential(
+            t.nn.Conv2d(in_channels=1, out_channels=4, kernel_size=3, stride=1),
+            t.nn.ReLU(),
+            t.nn.MaxPool2d(kernel_size=2),
+            
+            t.nn.Conv2d(in_channels=4, out_channels=8, kernel_size=3, stride=1),
+            t.nn.ReLU(),
+            t.nn.MaxPool2d(kernel_size=2)
+        )
+        
+        self.fc = t.nn.Linear(8 * 5 * 5, 10)
+        
+    
+    def forward(self, x):
+        out = self.layers(x)
+        # print(out.shape)
+        out = out.view(out.shape[0], -1)
+        out = self.fc(out)
+        
+        return out
